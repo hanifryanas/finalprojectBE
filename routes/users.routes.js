49c6c5caba861589c31beda37129a24b99339a02
hanif ryanas <hanifryanas@gmail.com>
2022-06-14 18:23:10 +0700

    5th commit

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,8 +3,8 @@ const controllerUsers = require('../controllers/users.controller.js');
 const Middleware = require('../middleware/checkAuth.middleware.js');
 
 router.get('/id/:id', controllerUsers.getUserById);
-router.post('/', controllerUsers.createUser);
-router.post('/login', controllerUsers.loginUser);
+router.post('/signup', controllerUsers.createUser);
+router.post('/signin', controllerUsers.loginUser);
 router.put('/id/:id', Middleware.checkAuth, controllerUsers.updateUser);
 router.delete('/id/:id', Middleware.checkAuth, controllerUsers.deleteUser);
 
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5010;
+const port = process.env.PORT;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const cors = require('cors');
+
+app.use(cors(
+    {
+        origin: 'http://localhost:3000',
+        credentials: true
+    }
+));
 
 app.use('/user', require('./routes/users.routes.js'));
 app.use('/user/id', require('./routes/products.routes.js'));
