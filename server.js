4cbb20b00437a7a5b16cabe3751d74476c6ec332
hanif ryanas <hanifryanas@gmail.com>
2022-06-17 20:31:10 +0700

    finally revs

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 app.use(cors(
     {
-        origin: 'http://localhost:3000',
+        origin: 'http://melelang.herokuapp.com',
         credentials: true
     }
 ));
